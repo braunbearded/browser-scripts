6badd562b6b40b04a12799fc8974d1ebac45a1f9
stefan <braunbearded@gmail.com>
2020-02-21 13:58:54 +0100

    add pretty auas and move js files in folder

---
+++ pretty-auas/README.md
@@ -0,0 +1,18 @@
+# PrettyAUAS
+
+Diese Userscript erweitert die Seite https://auas.cs.uni-duesseldorf.de um ein paar Zusatzfunktionalitäten
+
+1. Die Module für die man im AUAS angemeldet ist, wird in der Übersicht einen Button (sobald das Modul mit der Maus überfahren wird) hinzugefügt, mit dem den Inhalt des Moduls aus- und eingeblenden werden kann.
+   Die Einstellungen (welche Module eingeblendet bzw. ausgeblendet sind) werden im Localstorage des Browsers gespeichert und sind somit auch nach Neustart des Browser vorhanden.
+   
+2. Autofocus des E-Mailfeldes auf der Startseite
+
+# Installation
+1. Chrome Browser installieren
+2. Tampermonkey Erweiterung installieren
+3. Userscript in Tampermonkey einbinden
+
+# Test
+Das Userscript wurde bisher nur auf Windows Chrome Version 65.0.3325.181 (Offizieller Build) (64-Bit) für Tampermonkey gestestet.
+
+License MIT
---
+++ pretty-auas/prettyauas.js
@@ -0,0 +1,93 @@
+// ==UserScript==
+// @name         PrettyAUAS
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  Adds some new functionality to auas
+// @author       braunbearded
+// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.18.2/babel.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.16.0/polyfill.js
+// @match        https://auas.cs.uni-duesseldorf.de/*
+// @downloadURL  FIXME
+// ==/UserScript==
+
+/* jshint ignore:start */
+var inline_src = (<><![CDATA[
+/* jshint ignore:end */
+/* jshint esnext: false */
+/* jshint esversion: 6 */
+
+let courses = document.getElementsByTagName("fieldset");
+let overview = document.getElementsByClassName("icon-plus-circled").length > 0;
+let mailInput = document.getElementById("LoginEmail");
+
+if (courses.length > 0 && overview) {
+    for (let i = 0; i < courses.length; i++) {
+        let course = courses[i];
+        let children = course.children;
+
+        //is course hidden from the start?
+        let courseName = course.children[0].textContent;
+        let hidden = localStorage.getItem(courseName);
+        if (hidden === null || hidden === "false") hidden = false;
+        else hidden = true;
+
+
+        //add/show collabs button
+        let legend = course.children[0];
+        let expand = document.createElement("Button");
+        let expandText = document.createTextNode("hide/collaps");
+        expand.appendChild(expandText);
+        legend.appendChild(expand);
+        if (!hidden) expand.style.display = "none";
+
+        //restore last session
+        if (hidden === true) {
+            for (let child of Array.from(children)) {
+                if (child != legend) {
+                    child.style.display = "none";
+                }
+            }
+        }
+
+        course.addEventListener("mouseenter", event => {
+            expand.style.display = "initial";
+        });
+
+        course.addEventListener("mouseleave", event => {
+            if (expand && !hidden) expand.style.display = "none";
+        });
+
+        expand.addEventListener("click", event => {
+            if (hidden) {
+                for (let child of Array.from(children)) {
+                    if (child != legend && child != expand) {
+                        if (child.tagName === "TABLE") {
+                            child.style.display = "table";
+                        } else {
+                            child.style.display = "block";
+                        }
+                    }
+                }
+                hidden = false;
+                localStorage.setItem(courseName, false);
+            } else {
+                for (let child of Array.from(children)) {
+                    if (child != legend) {
+                        child.style.display = "none";
+                    }
+                }
+                hidden = true;
+                localStorage.setItem(courseName, true);
+            }
+        });
+    }
+} else if (mailInput){
+    mailInput.focus();
+    console.log("Bitte anmelden");
+}
+
+/* jshint ignore:start */
+]]></>).toString();
+var c = Babel.transform(inline_src, { presets: [ "es2015", "es2016" ] });
+eval(c.code);
+/* jshint ignore:end */
