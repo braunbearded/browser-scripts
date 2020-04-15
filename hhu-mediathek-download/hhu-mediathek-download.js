cbc26231357f77da4b7ea5821d8390cf33f1a220
braunbearded <braunbearded@gmail.com>
2020-04-15 14:24:56 +0200

    add: new midathek script

---
+++ hhu-mediathek-download/hhu-mediathek-download.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         hhu-mediathek-download
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  create download link for hhu mediathek videos
+// @author       braunbearded
+// @match        https://mediathek.hhu.de/watch/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let jwplayer = document.querySelector("#mt_watch_player > script:nth-child(3)").text.replace(/[\n\r\t]|  /g, "");
+    //let source = jwplayer.match(/sources: \[ *({ *file: *'[a-zA-Z0-9\/\._]*' *, *label: *'[a-zA-Z0-9 öÖäÄüÜ()]*' *(, *default: *'[a-z]*' *)?},? *)*\]/g);
+    let urls = jwplayer.match(/\/movies\/[a-zA-Z0-9]+\/v_\d+\.(mp4|webm)/g);
+    let download_box = document.createElement("div");
+    download_box.className = "watch-video";
+    let parent = document.querySelector("#mt_watch_video_container");
+    let download_header = document.createElement("h3");
+    download_header.innerHTML = "Downloads";
+    let download_list = document.createElement("ul");
+    download_box.appendChild(download_header);
+    for (let i = 0; i < urls.length; i++) {
+        let li = document.createElement("li");
+        let url = document.createElement("a");
+        url.setAttribute("href", urls[i]);
+        url.setAttribute("download", "download");
+        url.innerHTML = urls[i];
+
+        li.appendChild(url);
+        download_list.appendChild(li);
+    }
+    download_box.appendChild(download_list);
+    parent.appendChild(download_box);
+})();
